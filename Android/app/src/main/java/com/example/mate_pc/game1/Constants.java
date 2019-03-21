2a1d87d3d32175edfd1919d790143cca8e45fe7f
Worrador <a.poleczki@gmail.com>
2019-03-21 20:24:00 +0100

    logcat says the shared preferences part is ok, but still the highlighting f the images doesnt work properly (no matter what, after opening the fourth image is highlighted)

--- Android/app/src/main/java/com/example/mate_pc/game1/Constants.java
+++ Android/app/src/main/java/com/example/mate_pc/game1/Constants.java
@@ -4,8 +4,10 @@ public abstract class Constants {
 
     final static String controlSettingsKey = "controlSettings_isJoystick";
     final static String soundSettingsKey = "soundSettings_isSound";
+    final static String backgroundSettingsKey = "backgroundSettings_chosenBackground";
     final static String controlSettings = "controlSettings";
     final static String soundSettings = "soundSettings";
+    final static String backgroundSettings = "backgroundSettings";
     public final static int START_SETTINGS_CODE = 121;
     final static int START_CONNECTOR_CODE = 314;
     public final static int RESULT_CODE_SETTINGS_MAY_CHANGED = 0x01;
